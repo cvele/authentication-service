50bf6bb9b4691ce02bb0fdfabc0550aa01b88b2c
Vladimir Cvetic <vladimir@ferdinand.rs>
2023-02-26 14:39:39 +0100

    Migrate to os.ReadFile

--- internal/openapi/openapi.go
+++ internal/openapi/openapi.go
@@ -1,20 +1,18 @@
 package openapi
 
 import (
-	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 // OpenAPIHandler returns an HTTP handler that serves the OpenAPI specification for the service.
 func OpenAPIHandler(w http.ResponseWriter, r *http.Request) {
-	swaggerBytes, err := ioutil.ReadFile("docs/swagger.json")
+	swaggerBytes, err := os.ReadFile("docs/swagger.json")
 	if err != nil {
 		http.Error(w, "failed to read OpenAPI specification", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	if _, err := w.Write(swaggerBytes); err != nil {
-		http.Error(w, "failed to write OpenAPI specification", http.StatusInternalServerError)
-	}
+	w.Write(swaggerBytes)
 }
