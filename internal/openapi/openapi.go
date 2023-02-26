390d6cc301f12a4025baf39e753acd319f4880a8
Vladimir Cvetic <vladimir@ferdinand.rs>
2023-02-26 14:38:16 +0100

    Fixing lint errors

--- internal/openapi/openapi.go
+++ internal/openapi/openapi.go
@@ -7,7 +7,6 @@ import (
 
 // OpenAPIHandler returns an HTTP handler that serves the OpenAPI specification for the service.
 func OpenAPIHandler(w http.ResponseWriter, r *http.Request) {
-
 	swaggerBytes, err := ioutil.ReadFile("docs/swagger.json")
 	if err != nil {
 		http.Error(w, "failed to read OpenAPI specification", http.StatusInternalServerError)
@@ -15,5 +14,7 @@ func OpenAPIHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(swaggerBytes)
+	if _, err := w.Write(swaggerBytes); err != nil {
+		http.Error(w, "failed to write OpenAPI specification", http.StatusInternalServerError)
+	}
 }
